991c288e6a1da20fc4b73f453751c7bcd267c0b6
Jack Stanek <stane064@umn.edu>
2016-07-19 13:59:24 -0500

    Initial commit

---
+++ process.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+import csv
+
+def get_best_matches(csvfile):
+    FIRST_ID = 0
+    SECOND_ID = 1
+    SCORE = 7
+
+    best = []
+    prevbestid = ''
+
+    for row in csvfile:
+        if row[FIRST_ID] == row[SECOND_ID]:
+            pass
+        else:
+            if row[FIRST_ID] != prevbestid:
+                best.append(row)
+                prevbestid = row[FIRST_ID]
+
+def main():
+    if len(sys.argv) != 2:
+        print('Wrong number of arguments')
+    else:
+        filename = sys.argv[1]
+
+        bests = []
+
+        try:
+            with open(filename) as csvin:
+                csvfile = csv.reader(csvin)
+                bests = get_best_matches(csvfile)
+
+        except OSError:
+            print('Could not open file {}'.format(filename))
+
+        for row in bests:
+            sys.stdout.write('\t'.join(row))
+
+if __name__ == '__main__':
+    main()
